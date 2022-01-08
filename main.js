dd610727277f85062e1b0199e36ed349a9ace106
Ryota Sakaguchi <mailkuluna@gmail.com>
2022-01-08 17:02:02 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+const debug = true;
+
+/** @type HTMLButtonElement */
+const button = document.getElementById("draw");
+/** @type HTMLImageElement */
+const image = document.getElementById("image");
+/** @type HTMLHeadingElement */
+const title = document.getElementById("title");
+/** @type HTMLParagraphElement */
+const description = document.getElementById("description");
+
+let index = 0;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const lastIndex = localStorage.getItem("index");
+  const weekday = localStorage.getItem("weekday");
+
+  if (weekday !== null && index !== null) {
+    const nowWeekDay = new Date().getDay().toString();
+    if (weekday === nowWeekDay) {
+      index = parseInt(lastIndex);
+
+      if (!debug) {
+        button.style.display = "none";
+      }
+      showTodayCard();
+    }
+  }
+});
+
+function roulette() {
+  button.style.display = "none";
+
+  const handle = setInterval(() => draw(), 50);
+  // 3秒後にルーレットを止める
+  setTimeout(() => {
+    clearInterval(handle);
+
+    showTodayCard();
+    saveLocalStorage();
+
+    if (debug) {
+      button.style.display = "";
+    }
+  }, 3000);
+}
+
+function draw() {
+  index = Math.trunc(Math.random() * 5);
+  image.src = `img/img${index}.png`;
+}
+
+function showTodayCard() {
+  image.src = `img/img${index}.png`;
+
+  switch (index) {
+    case 0:
+      title.innerText = "納豆ご飯";
+      description.innerText = "朝ごはんはやっぱり納豆ご飯！";
+      break;
+    case 1:
+      title.innerText = "おもち";
+      description.innerText = "この時期無性に食べたくなるおもち！\nどれだけ伸ばせるかな？";
+      break;
+    case 2:
+      title.innerText = "にくまん";
+      description.innerText = "意外と熱い！やけどには気をつけて！";
+      break;
+    case 3:
+      title.innerText = "タピオカ";
+      description.innerText = "ブームが過ぎた？今だからこそタピろう！";
+      break;
+    case 4:
+      title.innerText = "アイスクリーム";
+      description.innerText = "冬だからこそアイスクリームがおいしい！";
+      break;
+  }
+}
+
+function saveLocalStorage() {
+  localStorage.setItem("index", index);
+  localStorage.setItem("weekday", new Date().getDay());
+}
